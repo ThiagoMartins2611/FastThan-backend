492d3c10bdef4b757ed57a62c5b5d917fecedc39
Thiago Costa Martins <thiagogames2611@gmail.com>
2025-10-28 01:31:53 -0300

    login and add User AuthRequest to Request modify

--- src/appLogic/users/userController.ts
+++ src/appLogic/users/userController.ts
@@ -1,5 +1,5 @@
 
-import { Response } from "express"
+import { Request, Response } from "express"
 import db from "../../database/mongo_database.js";
 import UserEntity from "./userEntity.js";
 import bcrypt from "bcrypt"
@@ -18,7 +18,7 @@ function randomNumber(min:number, max:number) {
 
 class UserController{
 
-    async add(req:AuthRequest, res:Response){
+    async add(req:Request, res:Response){
 
         const {name, age, email, key, adm} = req.body as {name:string, age:number, email:string, key:string, adm:boolean};
         
@@ -56,8 +56,9 @@ class UserController{
         
     }
 
-    async login(req:AuthRequest, res:Response){
+    async login(req:Request, res:Response){
         const {email, key} = req.body as {email:string, key:string}
+
         if(!(email || key)) return res.status(400).send({mensagem: "Email e key são obrigatorios"});
 
         const user = await db.collection<UserEntity>("users").findOne({email});
